24c4729b2eaad10b4b63015594c9ba9051a83520
hakobe <hakobe@gmail.com>
2018-01-03 01:40:42 +0900

    also export list

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 /* @flow */
 
-import Types from './types'
+import { Types, TypesList } from './types'
 import { createPokemon } from './pokemon'
 import { calc } from './effectiveness'
 
 export default {
   Types: Types,
+  TypesList: TypesList,
   createPokemon: createPokemon,
   calcEffectiveness: calc
 }
